bf151d5733f71a9f324bb5a573eb145bc98b3ca0
rentianhe <596106517@qq.com>
2019-12-06 10:29:05 +0800

    rentianhe_commit_deleteAd

--- malldemo/oomall/src/main/java/xmu/oomall/controller/AdController.java
+++ malldemo/oomall/src/main/java/xmu/oomall/controller/AdController.java
@@ -2,11 +2,6 @@ package xmu.oomall.controller;
 
 import com.alibaba.druid.support.json.JSONUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import xmu.oomall.domain.Ad;
 import xmu.oomall.service.impl.AdService;
@@ -14,7 +9,7 @@ import xmu.oomall.util.ResponseUtil;
 
 import java.util.List;
 
-@Transactional(rollbackFor = Exception.class)
+
 @RestController
 @RequestMapping("")
 public class AdController {
@@ -43,5 +38,22 @@ public class AdController {
     @DeleteMapping("/ads/{id}")
     public void deleteAdbyId(Integer id){
         adService.deleteAdbyId(id);
+    }
+
+    /**
+    * @Description: 管理员添加广告
+    * @Param: [ad]
+    * @return: java.lang.Object
+    * @Author: Zhou Linhui
+    * @Date: 2019/12/5
+    */
+    @PostMapping("/ads")
+    public Object addAds(@RequestBody Ad ad) {
+//        Object error = validate(ad);
+//        if (error != null) {
+//            return error;
+//        }
+        adService.addAds(ad);
+        return ResponseUtil.ok(ad);
     }
 }
