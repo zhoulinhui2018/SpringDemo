9f7eb991abd231d5f1cfcb1800d1778b72a93f17
Jessssssi <3267935606@qq.com>
2019-12-06 14:45:54 +0800

    commit by xuhuangchao GET /goods 实现 POST /goods实现

---
+++ malldemo/oomall/src/main/java/xmu/oomall/controller/GoodController.java
@@ -0,0 +1,46 @@
+package xmu.oomall.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xmu.oomall.domain.goods.Goods;
+import xmu.oomall.service.IGoodsService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("")
+public class GoodController {
+    @Autowired
+    private IGoodsService goodsService;
+
+    /**
+     * @Description: 用户获得商品列表
+     * @Param: []
+     * @return: List<Goods>
+     * @Author: Xu Huangchao
+     * @Date: 2019/12/6
+     */
+    @GetMapping("/goods")
+    public List<Goods> getAllGoods()
+    {
+       List<Goods> AllGoods=goodsService.getGoodsList();
+       return AllGoods;
+    }
+
+    /**
+     * @Description: 管理员新建商品
+     * @Param: Goods
+     * @return:
+     * @Author: Xu Huangchao
+     * @Date: 2019/12/6
+     */
+    @PostMapping("/goods")
+    public void addNewGoods(Goods good)
+    {
+        goodsService.addGoods(good);
+    }
+
+}
---
+++ malldemo/oomall/src/test/java/xmu/oomall/dao/GoodDaoTest.java
@@ -0,0 +1,38 @@
+package xmu.oomall.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import xmu.oomall.OoMallApplication;
+import xmu.oomall.domain.goods.Goods;
+
+import java.util.List;
+
+@SpringBootTest(classes = OoMallApplication.class)
+public class GoodDaoTest {
+    @Autowired
+    private GoodsDao goodDao;
+
+    @Test
+    public void findAllGoodsTest()
+    {
+        List<Goods> goodsList=goodDao.getGoodList();
+        for(int i=0;i<goodsList.size();i++)
+        {
+            Goods good=goodsList.get(i);
+            System.out.println(good);
+        }
+    }
+    @Test
+    public void addGoodsTest()
+    {
+        Goods newgood=new Goods();
+        newgood.setId(300006);
+        newgood.setName("百褶裙");
+        newgood.setDetail("好看的裙子");
+        newgood.setBrandId(100001);
+        newgood.setGoodsCategoryId("100001");
+        goodDao.addGoods(newgood);
+        System.out.println("插入一条数据成功");
+    }
+}
