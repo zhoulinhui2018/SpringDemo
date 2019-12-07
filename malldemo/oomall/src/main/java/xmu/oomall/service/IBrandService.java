8bd064fbaef6c486ae6cfc0349d6fef4891b026b
rentianhe <596106517@qq.com>
2019-12-07 22:16:46 +0800

    rentianhe_commit_updategoods

---
+++ malldemo/oomall/src/main/java/xmu/oomall/service/IBrandService.java
@@ -0,0 +1,24 @@
+package xmu.oomall.service;
+
+import org.springframework.stereotype.Service;
+import xmu.oomall.domain.goods.Brand;
+
+import java.util.List;
+
+@Service
+public interface IBrandService {
+    /**
+    * @Description: 新建一个品牌信息
+    * @Param: [brand]
+    * @Author: Ren Tianhe
+    * @Date: 2019/12/6
+    */
+
+    public void addBrand(Brand newbrand);
+
+    public int updateBrand(Brand newbrand);
+
+    public int deleteBrand(Brand newbrand);
+
+    public Brand findBrandById(Integer id);
+}
