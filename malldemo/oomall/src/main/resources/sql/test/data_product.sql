abb055e631621017ef1b2aebf8823f3ab635244c
rentianhe <596106517@qq.com>
2019-12-09 21:47:47 +0800

    rentianhe_commit_updategoods

---
+++ malldemo/oomall/src/main/resources/sql/test/data_product.sql
@@ -0,0 +1,10 @@
+insert into product values (100001,'1','url','specification',100001,20,20,'2019-12-9 21:45:01','2019-12-9 21:45:01',0)
+insert into product values (100002,'2','url','specification',100001,30,20,'2019-12-9 21:45:01','2019-12-9 21:45:01',0)
+insert into product values (100003,'3','url','specification',100001,40,20,'2019-12-9 21:45:01','2019-12-9 21:45:01',0)
+insert into product values (100004,'4','url','specification',100001,50,20,'2019-12-9 21:45:01','2019-12-9 21:45:01',0)
+insert into product values (100005,'5','url','specification',100002,60,20,'2019-12-9 21:45:01','2019-12-9 21:45:01',0)
+insert into product values (100006,'6','url','specification',100002,70,20,'2019-12-9 21:45:01','2019-12-9 21:45:01',0)
+insert into product values (100007,'7','url','specification',100003,80,20,'2019-12-9 21:45:01','2019-12-9 21:45:01',0)
+insert into product values (100008,'8','url','specification',100003,90,20,'2019-12-9 21:45:01','2019-12-9 21:45:01',0)
+insert into product values (100009,'9','url','specification',100003,100,20,'2019-12-9 21:45:01','2019-12-9 21:45:01',0)
+
