eed3b4563705c992e52fe78c2d00b7b67b9bb1bf
zhoulinhui <1047043261@qq.com>
2019-12-12 17:43:21 +0800

    commit by zhoulinhui 新建了四个模块

---
+++ malldemo/oomall/topicModule/src/test/java/xmu/oomall/topic/TopicApplicationTests.java
@@ -0,0 +1,13 @@
+package xmu.oomall.topic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
